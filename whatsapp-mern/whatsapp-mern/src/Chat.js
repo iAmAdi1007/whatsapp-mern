3adf82c8f572c8e3c5fd8ac819873c61e6794079
Aditya Pratap <pratapaditya516@gmail.com>
2021-08-02 19:18:49 +0530

    Initial Commit

---
+++ whatsapp-mern/whatsapp-mern/src/Chat.js
@@ -0,0 +1,43 @@
+import { Avatar, IconButton } from '@material-ui/core';
+import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
+import React from 'react'
+import './Chat.css'
+
+function Chat() {
+    return (
+        <div className="chat">
+            <div className="chat__header">
+                <Avatar/>
+                <div className="chat__headerInfo">
+                    <h3>Room Name</h3>
+                    <p>Last Seen at...</p>
+                </div>
+
+                <div className="chat__headerRight">
+                <IconButton>
+                        <SearchOutlined />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile/>
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert/>
+                    </IconButton>
+                </div>
+            </div>
+            <div className="chat__body">
+                <p className="chat__message">
+                    <span className="chat__name">Aditya</span>
+                    
+                    This is a message
+                    <span className="chat__timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+                    
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Chat
---
+++ whatsapp-mern/whatsapp-mern/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAwYGdOOk75VibjgahZeKhlaLDQrL0hCF4",
+    authDomain: "whatsapp-mern-c094c.firebaseapp.com",
+    projectId: "whatsapp-mern-c094c",
+    storageBucket: "whatsapp-mern-c094c.appspot.com",
+    messagingSenderId: "403697344681",
+    appId: "1:403697344681:web:b1c2dcb361bfc7c19289b8"
+  };
